b0453be8bbbd2fe93a3eb74777213f2249ff0ed6
NaolShow <naolyt@gmail.com>
2021-10-19 13:35:49 +0200

    feat(unigeweb): add web utility class

---
+++ UnigeWebUtility/HttpWebUtility.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+
+namespace UnigeWebUtility {
+
+    public static class HttpWebUtility {
+
+        /// <summary>
+        /// Sets the http web request content to the specified content type and the UTF8 encoded string
+        /// </summary>
+        public static void SetContent(this HttpWebRequest request, string contentType, string content)
+            => SetContent(request, contentType, Encoding.UTF8.GetBytes(content));
+
+        /// <summary>
+        /// Sets the http web request content to the specified content type and byte array
+        /// </summary>
+        public static void SetContent(this HttpWebRequest request, string contentType, byte[] content) {
+
+            // Set the request content type and length
+            request.ContentType = contentType;
+            request.ContentLength = content.Length;
+
+            // Write the content to the request stream
+            using Stream stream = request.GetRequestStream();
+            stream.Write(content, 0, content.Length);
+
+        }
+
+        /// <summary>
+        /// Gets the content of the http web response
+        /// </summary>
+        public static string GetContent(this HttpWebResponse response) {
+
+            // Get the response stream
+            using Stream stream = response.GetResponseStream();
+
+            // Convert the stream to a stream reader
+            using StreamReader reader = new StreamReader(stream);
+
+            return reader.ReadToEnd();
+
+        }
+
+    }
+
+}
