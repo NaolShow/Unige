0dfea7a0a6a31c2b22899b5f9f747a2c8524960d
NaolShow <naolyt@gmail.com>
2021-10-26 13:33:59 +0200

    feat(ogesharp): add schedule and timeslot classes

---
+++ OgeSharp/Schedule/Schedule.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace OgeSharp {
+
+    /// <summary>
+    /// Stores information about time slots
+    /// </summary>
+    public class Schedule {
+
+        /// <summary>
+        /// Array of the time slots of the schedule
+        /// </summary>
+        public TimeSlot[] Slots { get; }
+
+        /// <summary>
+        /// DateTime at which the schedule starts
+        /// </summary>
+        public DateTime Start { get; }
+        /// <summary>
+        /// DateTime at which the schedule ends
+        /// </summary>
+        public DateTime End { get; }
+
+        /// <summary>
+        /// Calculate the total duration of all the time slots
+        /// </summary>
+        public TimeSpan Duration {
+
+            get {
+
+                // Initialize an empty time span
+                TimeSpan span = new TimeSpan();
+
+                // Loop through the time slots and add their duration
+                foreach (TimeSlot slot in Slots) span = span.Add(slot.Duration);
+                return span;
+
+            }
+
+        }
+
+        internal Schedule(DateTime start, DateTime end, int timeSlotsCount) {
+
+            // Initialize the time slot array
+            Slots = new TimeSlot[timeSlotsCount];
+
+            // Save the start and end dates
+            Start = start;
+            End = end;
+
+        }
+
+    }
+
+}
---
+++ OgeSharp/Schedule/TimeSlot.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace OgeSharp {
+
+    /// <summary>
+    /// Stores informations about a time slot
+    /// </summary>
+    public class TimeSlot {
+
+        /// <summary>
+        /// Regex that extracts the ressource type
+        /// </summary>
+        internal static Regex RessourceTypeRegex = new Regex(@"[\w]{2}\.[\w]{2}");
+
+        internal TimeSlot(dynamic slot) {
+
+            // Parse the start and end time
+            Start = DateTime.Parse(slot["start"]);
+            End = DateTime.Parse(slot["end"]);
+            Duration = End - Start;
+
+            // Get the teacher and room
+            string[] description = ((string)slot["title"]).Split('\n');
+            Teacher = description[2].Trim();
+            Room = description[1].Trim();
+
+            // Get the title
+            FullTitle = ((string)slot["description"]).Trim();
+
+        }
+
+        #region Dates and Times
+
+        /// <summary>
+        /// DateTime at which the class starts
+        /// </summary>
+        public DateTime Start { get; }
+        /// <summary>
+        /// DateTime at which the class ends
+        /// </summary>
+        public DateTime End { get; }
+        /// <summary>
+        /// TimeSpan that represents the duration of the class
+        /// </summary>
+        public TimeSpan Duration { get; }
+
+        #endregion
+
+        /// <summary>
+        /// Teacher's full name
+        /// </summary>
+        public string Teacher { get; }
+        /// <summary>
+        /// Room number/id
+        /// </summary>
+        public string Room { get; }
+
+        /// <summary>
+        /// Full title of the class
+        /// </summary>
+        public string FullTitle { get; }
+        /// <summary>
+        /// Associated ressource of the class
+        /// </summary>
+        public string Ressource => RessourceTypeRegex.Match(FullTitle)?.Groups[0].Value;
+
+    }
+
+}
