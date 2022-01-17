4708c6f0d795b32da764bcc2ccdd2d60c62573a9
NaolShow <naolyt@gmail.com>
2022-01-17 21:13:14 +0100

    feat(browser): use all decompression methods

--- UnigeWebUtility/FakeWebBrowser.cs
+++ UnigeWebUtility/FakeWebBrowser.cs
@@ -49,7 +49,7 @@ namespace UnigeWebUtility {
             request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
 
             // Enable the automatic decompression
-            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            request.AutomaticDecompression = DecompressionMethods.All;
 
             // Set the request cookies
             request.CookieContainer = Cookies;
